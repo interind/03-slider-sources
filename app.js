8982c8737fb62e1a1b52d991b349bf72a37dcf4b
Aleksander <interind@yandex.ru>
2021-06-15 18:09:59 +0300

    start

---
+++ app.js
@@ -0,0 +1,37 @@
+const buttonUp = document.querySelector('.up-button');
+const buttonDown = document.querySelector('.down-button');
+const sidebar = document.querySelector('.sidebar');
+const mainSlide = document.querySelector('.main-slide');
+const slidesCount = mainSlide.querySelectorAll('div').length - 1;
+const container = document.querySelector('.container');
+
+const heightVH = container.clientHeight;
+
+let activeSlideIndex = 0;
+
+
+sidebar.style.top = `-${slidesCount * 100}vh`;
+
+buttonUp.addEventListener('mousedown', () => {
+  changeSlide('up');
+})
+buttonDown.addEventListener('mousedown', () => {
+  changeSlide('down');
+})
+
+
+function changeSlide(direction) {
+  if (direction === 'up') {
+    activeSlideIndex++
+    if (activeSlideIndex > slidesCount) {
+      activeSlideIndex = 0;
+    }
+  } else if (direction === 'down') {
+    activeSlideIndex--;
+    if (activeSlideIndex < 0) {
+      activeSlideIndex = slidesCount;
+    }
+  }
+  mainSlide.style.transform = `translateY(-${activeSlideIndex * heightVH}px)`
+  sidebar.style.transform = `translateY(${activeSlideIndex * heightVH}px)`
+}
